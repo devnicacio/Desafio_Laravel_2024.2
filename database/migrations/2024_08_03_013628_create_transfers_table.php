2b3d20d3778a0f2422abdeb21a8f63b5f22cf21c
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-08-02 22:49:00 -0300

    Cria migration de transferências

---
+++ database/migrations/2024_08_03_013628_create_transfers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('sender')->references('id')->on('account');
+            $table->string('recipient');
+            $table->string('agency');
+            $table->double('value');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
