da48ac424b7e430e5be6df106b502c4a5578cc6d
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-08-02 21:41:43 -0300

    Cria migration gerentes

---
+++ database/migrations/2024_08_03_003454_create_managers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('managers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->foreign('account')->references('id')->on('accounts');
+            $table->foreign('admin')->references('id')->on('admins');
+            $table->string('address');
+            $table->string('photo');
+            $table->string('phoneNumber');
+            $table->date('birthdate');
+            $table->string('cpf');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('managers');
+    }
+};
