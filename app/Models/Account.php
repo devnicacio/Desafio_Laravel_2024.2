2d9812c2192e94bc1d40be12c9ec98d9a8dbb4d4
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-08-04 13:02:45 -0300

    Altera model de contas

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    protected $table = 'accounts';
+
+    protected $fillable = [
+        'agency',
+        'number',
+        'balance',
+        'transferLimit',
+        'password',
+    ];
+
+    public function user() {
+        return $this->belongsTo(CommonUser::class);
+    }
+
+    public $timestamps = false;
 }
