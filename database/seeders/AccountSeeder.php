feb60180444a6c8743dc9cf959b40ef25cb3847a
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-08-13 15:54:31 -0300

    Cria factory de admin

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -2,19 +2,14 @@
 
 namespace Database\Seeders;
 
-use App\Models\Adress;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Account;
-use App\Models\Address;
-use App\Models\Admin;
 
 class AccountSeeder extends Seeder
 {
     public function run(): void
     {
         Account::factory(10)->create();
-        Address::factory(10)->create();
-        Admin::factory(10)->create();
     }
 }
