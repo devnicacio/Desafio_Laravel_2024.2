dd4deef11febeb4aa5481bcbb94c6a9614a062c1
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-08-12 22:03:45 -0300

    Adiciona sobrecarga de método à função que gera números únicos

--- app/Helpers/generateUnicNumber.php
+++ app/Helpers/generateUnicNumber.php
@@ -6,8 +6,19 @@ use Illuminate\Support\Facades\DB;
 if(!function_exists('generateUnicNumber')){
     function generateUnicNumber($table, $column, $digits) {
         $faker = \Faker\Factory::create();
+        
         do{
             $number = strval($faker->randomNumber($digits, true));
+        } while(DB::table($table)->where($column, $number)->exists());
+
+        return $number;
+    }
+
+    function generateUnicNumber($table, $column, $digits, $format) {
+        $faker = \Faker\Factory::create();
+        
+        do{
+            $number = $faker->numerify($format);
         } while(DB::table($table)->where($column, $number)->exists());
 
         return $number;
