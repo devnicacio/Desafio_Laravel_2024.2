77d1877fafa016921402d2fab3140d727088589e
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-09-01 18:32:30 -0300

    Inicia dashboard de gerentes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,10 +11,8 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center">
+                    {{"Safebank"}}
                 </div>
             </div>
 
--- resources/views/manager/managerDashboard.blade.php
+++ resources/views/manager/managerDashboard.blade.php
@@ -1,17 +1,49 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight flex items-center">
+                {{ __('Gerente') }}
+            </h2>
+            <div>
+                <p class="text-sm">Agência: </p>
+                <p class="text-sm">Conta: </p>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center">
+                <div class="p-6 text-gray-900 text-lg">
+                    {{ __("Saldo: R$") }}
+                </div>
+                <div class="px-6">
+                    <x-primary-button>
+                        {{"Ver extrato"}}
+                    </x-primary-button>
+                </div>
+            </div>
+            <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-6">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center p-6">
+                    <p>Alalao</p>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center p-6">
+                    <p>Alalao</p>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center p-6">
+                    <p>Alalao</p>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center p-6">
+                    <p>Alalao</p>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center p-6">
+                    <p>Alalao</p>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center p-6">
+                    <p>Alalao</p>
                 </div>
             </div>
         </div>
+        
     </div>
 </x-app-layout>
