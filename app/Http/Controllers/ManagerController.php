6014b3a013169169f4bf24b23244a05c4256dc6c
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-09-06 23:49:34 -0300

    Finaliza crud usuários pelos managers

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -6,8 +6,11 @@ use App\Models\Account;
 use App\Models\Address;
 use App\Models\User;
 use Auth;
+use Carbon\Carbon;
 use Hash;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ManagerController extends Controller
 {
@@ -59,7 +62,11 @@ class ManagerController extends Controller
 
     public function updateManager(Request $request)
     {
+        $validAge = Carbon::now()->subYears(18)->format('d/m/Y');
+
         $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
             'country' => 'required|string|max:255',
             'postalCode' => 'required|string|max:255',
             'state' => 'required|string|max:255',
@@ -68,12 +75,13 @@ class ManagerController extends Controller
             'street' => 'required|string|max:255',
             'number' => 'required|integer|min:1',
             'complement' => 'nullable|string|max:255',
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
             'phoneNumber'=> 'required|string|max:255',
+            'birthdate' => 'required|date|before:' . $validAge,
             'cpf' => 'required|string|max:255',
-            'photo' => 'nullable|image|max:255',
+            'photo' => 'required|image|max:255',
             'password' => 'nullable|string|max:255'
+        ],[
+            'birthdate.before' => "O usuário precisa ter mais de 18 anos."
         ]);
 
         $manager = Auth::guard('manager')->user();
@@ -117,7 +125,11 @@ class ManagerController extends Controller
 
     public function updateUser(User $user, Request $request)
     {
+        $validAge = Carbon::now()->subYears(18)->format('d/m/Y');
+
         $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
             'country' => 'required|string|max:255',
             'postalCode' => 'required|regex:/^[0-9\-]+$/|max:255',
             'state' => 'required|string|max:255',
@@ -126,12 +138,13 @@ class ManagerController extends Controller
             'street' => 'required|string|max:255',
             'number' => 'required|integer|min:1',
             'complement' => 'nullable|string|max:255',
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
             'phoneNumber'=> 'required|string|max:255',
+            'birthdate' => 'required|date|before:' . $validAge,
             'cpf' => 'required|string|max:255',
             'photo' => 'nullable|image|max:255',
             'password' => 'nullable|string|max:255'
+        ],[
+            'birthdate.before' => "O usuário precisa ter mais de 18 anos."
         ]);
 
         $address = $user->address()->first();
@@ -196,7 +209,74 @@ class ManagerController extends Controller
 
     public function storeUser(Request $request)
     {
-        $request->session()->flash('msg', "Usuário excluído com sucesso!");
+        $validAge = Carbon::now()->subYears(18)->format('d/m/Y');
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'country' => 'required|string|max:255',
+            'postalCode' => 'required|regex:/^[0-9\-]+$/|max:255',
+            'state' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'neighborhood' => 'required|string|max:255',
+            'street' => 'required|string|max:255',
+            'number' => 'required|integer|min:1',
+            'complement' => 'nullable|string|max:255',
+            'phoneNumber'=> 'required|string|max:255',
+            'birthdate' => 'required|date|before:' . $validAge,
+            'cpf' => 'required|string|max:255',
+            'photo' => 'nullable|image|max:255',
+            'password' => 'required|string|max:255',
+            'agency' => 'required|integer|digits:4',
+            'transferLimit' => 'required|regex:/^\d+([.,]\d{1,2})?$/'
+        ], [
+            'birthdate.before' => "O usuário precisa ter mais de 18 anos."
+        ]);
+
+        if($request->file('photo')){
+            $path = "storage/" . $request->file('photo')->store('images','public');
+        }
+        else{
+            $photo = new File(public_path('images/safebank-default-profile-photo.png'));
+            $photoName = $photo->hashName();
+            Storage::disk('public')->putFileAs('images', $photo, $photoName);
+            $path = "storage/images/" . $photoName;
+        }
+
+        $manager = Auth::guard('manager')->user();
+
+        $account = Account::create([
+            'agency' => $request->agency,
+            'number' => generateUnicNumber('accounts', 'number', '#######'),
+            'balance' => 0,
+            'transferLimit' => $request->transferLimit
+        ]);
+
+        $address = Address::create([
+            'country' => $request->country,
+            'postalCode' => $request->postalCode,
+            'state' => $request->state,
+            'city' => $request->city,
+            'neighborhood' => $request->neighborhood,
+            'street' => $request->street,
+            'number' => $request->number,
+            'complement' => $request->complement,
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'account' =>  $account->id,
+            'manager' => $manager->id,
+            'address' => $address->id,
+            'phoneNumber'=> $request->phoneNumber,
+            'birthdate' => $request->birthdate,
+            'cpf' => $request->cpf,
+            'photo' => $path,
+        ]);
+
+        $request->session()->flash('msg', "Usuário criado com sucesso!");
 
         return redirect(route('manager-user-list'));
     }
